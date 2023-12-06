1ea6f21fe9333d04164d3b79c52f1b28477bbcca
Zekii <zach7505@gmail.com>
2023-12-06 23:59:22 +1100

    I did the first half, ran out of time for the second - ill do it at some point

--- Day5/main.py
+++ Day5/main.py
@@ -6,16 +6,6 @@ with open("./Day5/input.txt", "r") as file:
     for line_ind, line in enumerate(file):
         if line_ind == 0:
             initial_seeds = [int(s) for s in line.strip().split(" ")[1:]]
-            for i in range(0, len(initial_seeds), 2):
-                seeds += [
-                    seed
-                    for seed in range(
-                        initial_seeds[i], initial_seeds[i] + initial_seeds[i + 1]
-                    )
-                ]
-            print(seeds)
-            quit()
-            continue
 
         if "map" in line:
             in_map = True
@@ -25,12 +15,9 @@ with open("./Day5/input.txt", "r") as file:
             in_map = False
 
             for i, seed in enumerate(seeds):
-                print(i)
                 for c in convert:
                     if c[1] <= seed <= c[1] + (c[2]):
-                        print(f"found conversion {c} for seed {seed}")
                         seeds[i] = c[0] + (seed - c[1])
-                        print(f"output {seeds[i]}")
                         break
 
             convert = []
