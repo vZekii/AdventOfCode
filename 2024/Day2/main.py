3cf9afc3ad2f54def4bec21f120f19400a7b9858
Zekii <zach7505@gmail.com>
2024-12-05 22:40:58 +1100

    finished day 2

--- 2024/Day2/main.py
+++ 2024/Day2/main.py
@@ -34,50 +34,33 @@ def part1() -> int:
 def part2() -> int:
     inputs = []
 
+    def is_safe(levels):
+        # Check if the levels are all increasing or decreasing with differences of 1-3
+        increasing = all(
+            1 <= levels[i + 1] - levels[i] <= 3 for i in range(len(levels) - 1)
+        )
+        decreasing = all(
+            1 <= levels[i] - levels[i + 1] <= 3 for i in range(len(levels) - 1)
+        )
+        return increasing or decreasing
+
     with open("2024/Day2/input.txt", "r") as file:
         for line in file:
-            inputs.append([int(x) for x in line.strip().split(" ") if x])
+            inputs.append(list(map(int, line.split())))
 
-    safe = 0
-    for line in inputs:
-        ascending = True
-        prev = 0
-        canSkip = True
-        for i, num in enumerate(line):
-            if i == 0:
-                ascending = num < line[i + 1]
-                prev = num
-                continue
-            if ascending:
-                # if the number isnt ascending, break out of the loop
-                if num <= prev or num - prev > 3:
-                    if canSkip:
-                        canSkip = False
-                        continue
-                    elif not canSkip:
-                        if prev > num and num > line[i - 1] > line[i - 3]:
-                            prev = num
-                            continue
+    safe_count = 0
+    for report in inputs:
+        if is_safe(report):
+            safe_count += 1
+        else:
+            # Check if removing one level makes it safe
+            for i in range(len(report)):
+                new_report = report[:i] + report[i + 1 :]  # Remove the i-th level
+                if is_safe(new_report):
+                    safe_count += 1
+                    break  # Only need one valid removal to count as safe
 
-                    break
-                prev = num
-            else:
-                # if the number isnt descending, break out of the loop
-                if num >= prev or prev - num > 3:
-                    if canSkip:
-                        canSkip = False
-                        continue
-                    elif not canSkip:
-                        if prev < num and num < line[i - 1] < line[i - 3]:
-                            prev = num
-                            continue
-                    break
-                prev = num
-
-            if i == len(line) - 1:
-                safe += 1
-
-    return safe
+    return safe_count
 
 
 def main() -> None:
